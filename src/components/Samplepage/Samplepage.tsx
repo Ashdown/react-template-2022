d617c35b8f92af931b61cde236301490c928651f
Ashdown <rory.devane@gmail.com>
2022-08-18 11:56:53 +0100

    Added basic styling

--- src/components/Samplepage/Samplepage.tsx
+++ src/components/Samplepage/Samplepage.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {createUseStyles} from "react-jss";
 
-const Samplepage = () =>
-  <>
-    <h1>Sample page</h1>
-    <p>Sample page goes here</p>
-  </>
+const useStyles = createUseStyles(() => ({
+  header: {
+    fontFamily: 'roboto',
+    fontSize: 16,
+    fontWeight: 400
+  },
+  para: {
+    fontFamily: 'roboto',
+    fontSize: 12
+  }
+}))
+
+const Samplepage = () => {
+
+  const classes = useStyles()
+
+  return(<>
+    <h1 className={classes.header}>Sample page</h1>
+    <p className={classes.para}>Sample page goes here</p>
+  </>)
+}
 
 export default Samplepage
