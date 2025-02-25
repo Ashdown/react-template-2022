add6b1adb0fcb50082f8882d4de16959b13bd0ea
ashdown <rory.devane@gmail.com>
2025-02-25 12:10:59 +0000

    Updated xhr solution to fetch

--- src/services/useThingsQuery.ts
+++ src/services/useThingsQuery.ts
@@ -1,10 +1,9 @@
 import {useQuery} from "react-query";
-import axios from "axios";
 
 const useThingsQuery = () =>
-  useQuery(['things'], async () => {
-      const { data } = await axios.get('http://localhost:3000/things')
-      return data
+    useQuery(['things'], async () => {
+        const data = await fetch(`http://localhost:3000/things`).then(response => response.json())
+        return data
     })
 
 export default useThingsQuery
